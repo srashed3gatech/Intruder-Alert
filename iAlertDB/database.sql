f8e84843cf0a0af8fbc3e24a947cc8e6fb59f541
Yaling Wu <valence931215@gmail.com>
2016-11-18 13:03:19 -0500

    changed to update/delete cascade

--- iAlertDB/database.sql
+++ iAlertDB/database.sql
@@ -23,7 +23,7 @@ CREATE TABLE `SYSTEM_USER` (
   `email` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL DEFAULT 'password',
   PRIMARY KEY (`user_id`),
-  CONSTRAINT `fk_system_user_id` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_system_user_id` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -62,7 +62,7 @@ CREATE TABLE `RELATED_USER` (
   `user_id` varchar(15) NOT NULL,
   `conf_level_thresh` decimal(3,0) NOT NULL,
   PRIMARY KEY (`user_id`),
-  CONSTRAINT `fk_related_user_id` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_related_user_id` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -75,7 +75,7 @@ CREATE TABLE `RELATED_USER_PICTURE` (
   `pic_id` int(11) NOT NULL,
   `pic_path` varchar(256) NOT NULL,
   PRIMARY KEY (`user_id`,`pic_id`),
-  CONSTRAINT `fk_related_user_id_picture` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_related_user_id_picture` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -88,7 +88,7 @@ CREATE TABLE `FRAME` (
   `frame_num` bigint(100) NOT NULL,
   `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`video_id`,`frame_num`),
-  CONSTRAINT `fk_frame_videoid` FOREIGN KEY (`video_id`) REFERENCES `VIDEO` (`video_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_frame_videoid` FOREIGN KEY (`video_id`) REFERENCES `VIDEO` (`video_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -105,8 +105,8 @@ CREATE TABLE `CORRESPONDS_TO` (
   PRIMARY KEY (`user_id`,`video_id`,`frame_num`),
   KEY `fk_corr_videoid_idx` (`video_id`),
   KEY `fk_corr_framenum_idx` (`frame_num`),
-  CONSTRAINT `fk_corr_userid` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_corr_videoid_frameid` FOREIGN KEY (`video_id`) REFERENCES `FRAME` (`video_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_corr_userid` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_corr_videoid_frameid` FOREIGN KEY (`video_id`) REFERENCES `FRAME` (`video_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -131,8 +131,8 @@ CREATE TABLE `CONFIGS` (
   `deferral_time_sec` int(11) NOT NULL DEFAULT '60',
   PRIMARY KEY (`user_id`,`cate_name`),
   KEY `fk_config_alarm_category_idx` (`cate_name`),
-  CONSTRAINT `fk_config_alarm_category` FOREIGN KEY (`cate_name`) REFERENCES `ALARM_CATEGORY` (`cate_name`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_config_user_id` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_config_alarm_category` FOREIGN KEY (`cate_name`) REFERENCES `ALARM_CATEGORY` (`cate_name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_config_user_id` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -148,7 +148,7 @@ CREATE TABLE `ALARM` (
   `cate_name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`alarm_id`),
   KEY `fk_category_name_idx` (`cate_name`),
-  CONSTRAINT `fk_category_name` FOREIGN KEY (`cate_name`) REFERENCES `ALARM_CATEGORY` (`cate_name`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_category_name` FOREIGN KEY (`cate_name`) REFERENCES `ALARM_CATEGORY` (`cate_name`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -163,8 +163,8 @@ CREATE TABLE `GENERATED_FROM` (
   PRIMARY KEY (`video_id`,`frame_num`),
   KEY `fk_genfrom_frameno_idx` (`frame_num`),
   KEY `fk_genfrom_alarmid_idx` (`alarm_id`),
-  CONSTRAINT `fk_genfrom_alarmid` FOREIGN KEY (`alarm_id`) REFERENCES `ALARM` (`alarm_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_genfrom_frameno` FOREIGN KEY (`video_id`, `frame_num`) REFERENCES `FRAME` (`video_id`, `frame_num`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_genfrom_alarmid` FOREIGN KEY (`alarm_id`) REFERENCES `ALARM` (`alarm_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_genfrom_frameno` FOREIGN KEY (`video_id`, `frame_num`) REFERENCES `FRAME` (`video_id`, `frame_num`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -179,8 +179,8 @@ CREATE TABLE `SENT_TO` (
   `status` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`,`alarm_id`),
   KEY `fk_sentto_alarmid_idx` (`alarm_id`),
-  CONSTRAINT `fk_sentto_alarmid` FOREIGN KEY (`alarm_id`) REFERENCES `ALARM` (`alarm_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_sentto_userid` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_sentto_alarmid` FOREIGN KEY (`alarm_id`) REFERENCES `ALARM` (`alarm_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_sentto_userid` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
